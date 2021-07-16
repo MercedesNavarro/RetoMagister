3ff04454127bec2bdbd33ab42708dacf4b7544fa
MercedesNavarro <m.navarroc@live.com>
2021-07-16 18:27:18 +0200

    React environment created

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+const basePath = __dirname;
+
+module.exports = {
+  context: path.join(basePath, 'src'),
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  devtool: 'eval-source-map',
+  entry: ['./index.tsx', './styles.scss'],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /..\/node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /..\/node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  mode: 'development',
+  devServer: {
+    stats: 'errors-only',
+  },
+};
